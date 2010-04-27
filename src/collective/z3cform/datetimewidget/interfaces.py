aff62cd084352075a318154e17399d9aba9fc463
Carsten Senger <senger@rehfisch.de>
2010-04-27 13:40:08 +0000

    * add show_today_link to the IDateWidget interface * whitespace cleanup
    svn path=/collective.z3cform.datetimewidget/trunk/; revision=116241

--- src/collective/z3cform/datetimewidget/interfaces.py
+++ src/collective/z3cform/datetimewidget/interfaces.py
@@ -1,4 +1,4 @@
-#-*- coding: utf-8 -*- 
+#-*- coding: utf-8 -*-
 
 #############################################################################
 #                                                                           #
@@ -28,26 +28,42 @@ import zope.schema
 from i18n import MessageFactory as _
 
 
+# Fields
+
 class IDateField(zope.schema.interfaces.IDate):
     """ Special marker for date fields that use our widget """
+
 
 class IDatetimeField(zope.schema.interfaces.IDatetime):
     """ Special marker for datetime fields that use our widget """
 
 
+# Widgets
+
 class IDateWidget(z3c.form.interfaces.IWidget):
     """ Date widget marker for z3c.form """
 
+    show_today_link = zope.schema.Bool(
+        title=u'Show "today" link',
+        description=(u'show a link that uses javascript to inserts '
+                     u'the current date into the widget.'),
+        default=False,
+        )
+
+
 class IDatetimeWidget(z3c.form.interfaces.IWidget):
     """ Datetime widget marker for z3c.form """
+
 
 class IMonthYearWidget(z3c.form.interfaces.IWidget):
     """ MonthYear widget marker for z3c.form """
 
 
+# Errors
+
 class DateValidationError(zope.schema.ValidationError):
     __doc__ = _(u'Please enter a valid date.')
 
+
 class DatetimeValidationError(zope.schema.ValidationError):
     __doc__ = _(u'Please enter a valid date and time.')
-
