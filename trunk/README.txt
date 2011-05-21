887432666e142c640a0153545e25ed9bfe973654
Steve McMahon <steve@reidmcmahon.com>
2011-05-21 14:37:14 +0000

    Rapture sprint experiments
    svn path=/collective.z3cform.datetimewidget/branches/rapture-sprint/; revision=239768

---
+++ trunk/README.txt
@@ -0,0 +1,33 @@
+Various z3c.form date and datetime widgets
+==========================================
+
+Improved datetime widgets for z3c.form
+
+    When the emphasis of the application you are designing is on speed and
+    speed alone, then the single field pattern might be your friend. It's
+    particularly useful for data entry applications in which the person
+    who has to enter the data will have to use the same input form time
+    and time again. 
+
+.. _Date Input Diversity: http://www.cornae.org/articles/date-input-diversity
+
+
+The ``collective.z3cform.datetimewidget`` library provides a form framework
+and widgets. This document ensures that we implement a widget for Date and
+Datetime field defined in ``zope.schema``. Take a look at the different widget 
+doctest files for more information about the widgets.
+
+
+TODO
+====
+
+ * create DateDeltaWidget and DatetimeDeltaWidget
+
+
+Contributors
+============
+
+ * Vincent Fretin - vincentfretin
+ * David Glick - davisagli
+ * Rok Garbas - garbas
+ * Carsten Senger - csenger
---
+++ trunk/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+version = '1.0.4'
+
+setup(name='collective.z3cform.datetimewidget',
+      version=version,
+      description="z3c.form date and datetime widgets",
+      long_description=open("README.txt").read()+"\n"+open("HISTORY.txt").read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='z3cform datetime widget',
+      author='Rok Garbas',
+      author_email='rok@garbas.si',
+      url='http://svn.plone.org/svn/collective/collective.z3cform.datetimewidget',
+      license='GPL',
+      packages = find_packages('src', exclude=['ez_setup']),
+      package_dir = {'':'src'},
+      namespace_packages=['collective', 'collective.z3cform'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'zope.i18n',
+          'z3c.form',
+          'zope.deprecation'
+      ],
+      extras_require=dict(test=[
+            'z3c.form[test]',
+            'zope.testing',
+            'zc.buildout',
+            'Zope2',]),
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
