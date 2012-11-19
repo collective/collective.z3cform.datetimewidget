3003f6f5d6332e6a0a1bba6a78467b18803f6771
Andreas Jung <yet@gmx.de>
2012-11-19 08:34:31 +0100

    Update src/collective/z3cform/datetimewidget/setuphandlers.py
    No debug output to stdout please

--- src/collective/z3cform/datetimewidget/setuphandlers.py
+++ src/collective/z3cform/datetimewidget/setuphandlers.py
@@ -12,12 +12,12 @@ def importVarious(context):
 
     site = context.getSite()
     
-    print "Enabling %s" % js_date_input_id
+#    print "Enabling %s" % js_date_input_id
     jsrr = getToolByName(site, 'portal_javascripts')
     if js_date_input_id in jsrr.getResourceIds():
         jsrr.updateScript(js_date_input_id, enabled=True)
 
-    print "Enabling %s" % css_date_input_id
+#    print "Enabling %s" % css_date_input_id
     cssrr = getToolByName(site, 'portal_css')
     if css_date_input_id in cssrr.getResourceIds():
         cssrr.updateStylesheet(css_date_input_id, enabled=True)
