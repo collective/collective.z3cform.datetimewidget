fb4f5446eaded555722a6d5e852bdf5961c3e16e
Maurits van Rees <maurits@vanrees.org>
2020-01-28 16:57:57 +0100

    Updated classifiers.
    I guess we are only compatible with Python 2.7.

--- setup.py
+++ setup.py
@@ -8,7 +8,9 @@ setup(name='collective.z3cform.datetimewidget',
       long_description=open("README.rst").read() + "\n" +
           open("HISTORY.rst").read(),
       classifiers=[
+        "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='z3cform datetime widget',
